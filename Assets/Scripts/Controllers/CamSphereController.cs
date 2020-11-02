c22db86369a19c41e9f62dc1d8b68b7e3a980e48
Sergio Murillo Mendez <sergio.murillo.mendez@gmail.com>
2020-11-02 23:38:33 +0100

    Integración Modelos + CamSphereCOntroller

--- Assets/Scripts/Controllers/CamSphereController.cs
+++ Assets/Scripts/Controllers/CamSphereController.cs
@@ -7,23 +7,15 @@ public class CamSphereController : MonoBehaviour
     private MeshRenderer[] renders;
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Enter " + other);
-        other.GetComponent<MeshRenderer>().enabled = false;
-
         renders = other.GetComponentsInChildren<MeshRenderer>();
         foreach (var render in renders)
         {
             render.enabled = false;
         }
-        
-
     }
 
     private void OnTriggerExit(Collider other)
-    {
-        Debug.Log("Exit " + other);
-        other.GetComponent<MeshRenderer>().enabled = true;
-        
+    {               
         renders = other.GetComponentsInChildren<MeshRenderer>();
         foreach (var render in renders)
         {
