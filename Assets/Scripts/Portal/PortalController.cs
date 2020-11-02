eb209a31cff07154bb56a75e710b2673b2591df2
alexbarra <alexbarra00@gmail.com>
2020-11-02 22:43:56 +0100

    Portal Prefab & PortalController
    -Portal Prefab & PortalController

--- Assets/Scripts/Portal/PortalController.cs
+++ Assets/Scripts/Portal/PortalController.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PortalController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform otherPortal;
+    public GameObject otherPortal2;
+    private PortalController opController;
+
+    private bool cancelTeleport;
+
+    public bool alreadyTeleported { get; set; }
+
     void Start()
     {
-        
+        opController = otherPortal2.GetComponent<PortalController>();
+        cancelTeleport = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (opController.alreadyTeleported) cancelTeleport = true;
+        else cancelTeleport = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            if (!cancelTeleport)
+            {
+                other.gameObject.transform.position = otherPortal.position;
+               // other.gameObject.transform.rotation = otherPortal.rotation;
+                alreadyTeleported = true;
+            }
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            alreadyTeleported = false;
+        }
     }
 }
