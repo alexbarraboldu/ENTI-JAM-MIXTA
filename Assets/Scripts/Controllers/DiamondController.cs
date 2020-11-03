fa56681f77800040503699efd7dc9cc21585319f
Sergio Murillo Mendez <sergio.murillo.mendez@gmail.com>
2020-11-03 09:33:19 +0100

    ProGrids + TMP updated + Prefabs modelos

---
+++ Assets/Scripts/Controllers/DiamondController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondController : MonoBehaviour
+{
+    [Range(5f,20f)]
+    public float rotation;
+
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.forward, rotation * Time.fixedDeltaTime);
+        
+        
+    }
+}
---
+++ Assets/Scripts/Controllers/ShowColliderEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowColliderEditor : MonoBehaviour
+{
+    private BoxCollider boxCollider;
+    
+    private void OnDrawGizmos()
+    {
+        boxCollider = gameObject.GetComponent<BoxCollider>();
+        Gizmos.DrawWireCube(transform.position, boxCollider.size);
+    }
+    
+}
