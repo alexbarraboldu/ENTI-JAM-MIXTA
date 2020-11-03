c5734edb29a4a60d12430601b1f117056505a5a9
Sergio Murillo Mendez <sergio.murillo.mendez@gmail.com>
2020-11-03 11:59:00 +0100

    PropsPendientes+TemplateScene

---
+++ Assets/Scripts/Controllers/PowerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerController : MonoBehaviour
+{
+    [Range(5f, 20f)]
+    public float rotation;
+
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.forward, rotation * Time.fixedDeltaTime);
+    }
+}
