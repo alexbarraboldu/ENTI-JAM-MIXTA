8963d3188062347ba385adba7f9e66b40058a369
Sergio Murillo Mendez <sergio.murillo.mendez@gmail.com>
2020-11-03 11:09:38 +0100

    Modelos

---
+++ Assets/Scripts/Controllers/ShowCollidersIslandsEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCollidersIslandsEditor : MonoBehaviour
+{
+    private float radius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        radius = gameObject.GetComponent<CapsuleCollider>().radius;
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
